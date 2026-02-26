6aeb97fd17a964a5ea078947dd3802ba5be869c6
Eduardo_A. <edu.crecenzio.1003@gmail.com>
2026-02-26 18:54:13 -0300

    make the json of the api response pretty print

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -6,7 +6,72 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+
 public class PrincipalComBusca {
+    public static String prettyPrintJson(String json) {
+        if (json == null || json.isBlank()) return json;
+
+        StringBuilder out = new StringBuilder();
+        int indent = 0;
+        boolean inString = false;
+        boolean escape = false;
+
+        for (int i = 0; i < json.length(); i++) {
+            char c = json.charAt(i);
+
+            if (inString) {
+                out.append(c);
+                if (escape) {
+                    escape = false;
+                } else if (c == '\\') {
+                    escape = true;
+                } else if (c == '"') {
+                    inString = false;
+                }
+                continue;
+            }
+
+            switch (c) {
+                case '"':
+                    inString = true;
+                    out.append(c);
+                    break;
+
+                case '{':
+                case '[':
+                    out.append(c).append('\n');
+                    indent++;
+                    appendIndent(out, indent);
+                    break;
+
+                case '}':
+                case ']':
+                    out.append('\n');
+                    indent = Math.max(0, indent - 1);
+                    appendIndent(out, indent);
+                    out.append(c);
+                    break;
+
+                case ',':
+                    out.append(c).append('\n');
+                    appendIndent(out, indent);
+                    break;
+
+                case ':':
+                    out.append(c).append(' ');
+                    break;
+
+                default:
+                    if (!Character.isWhitespace(c)) out.append(c);
+                    break;
+            }
+        }
+        return out.toString();
+    }
+
+    private static void appendIndent(StringBuilder sb, int indent) {
+        sb.append("  ".repeat(Math.max(0, indent))); // 2 spaces per level
+    }
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -16,6 +81,7 @@ public class PrincipalComBusca {
         HttpResponse <String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+
+        System.out.println(prettyPrintJson(response.body()));
     }
 }
